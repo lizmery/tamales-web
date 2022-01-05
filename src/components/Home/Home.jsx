3f4804dd6d1ecf44a92f9df952da189e6c379102
lizmery <l.vigil096@gmail.com>
2022-01-05 10:54:41 -0800

    Create Home.jsx and Home.css

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <section className="home">
+            <div className="home-container">
+                <h1>Authentic Tamales</h1>
+                <h6>A taste of Oaxaca, made with the utmost care</h6>
+                <Link to="/menu" className="order-btn">Order Now</Link>
+            </div>
+        </section>
+    )
+}
+
+export default Home
