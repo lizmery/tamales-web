4dc10423f3eda9b82dcfae10a12e7d4c6595d21a
lizmery <l.vigil096@gmail.com>
2022-01-17 14:31:08 -0800

    update Navbar
    update navbar and upload images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import About from './components/About/About';
+import Menu from './components/Menu/Menu';
 import './App.css'
 
 const App = () => {
@@ -9,7 +11,20 @@ const App = () => {
     <>
       <Router>
         <Navbar />
-        <Home />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/about"
+            element={<About />}
+          />
+          <Route
+            path="/menu"
+            element={<Menu />}
+          />
+        </Routes>
       </Router>
     </>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,80 @@
 import React, { useState } from 'react'
 import { NavLink, Link } from 'react-router-dom';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import molcajete from '../../assets/molcajete2.png'
 import './Navbar.css';
 
 const Navbar = () => {
     const [click, setClick] = useState(false);
 
     const handleClick = () => setClick(!click);
-    
+
     return (
-        <nav className="navbar">
+        <header className={click ? "header active" : "header"}>
+            <nav className="nav container">
+                {/* Icons made by Freepik from flaticon.com */}
+                <NavLink exact to="/" className="nav-logo">
+                    <img src={molcajete} alt="molcajete" className="" />
+                </NavLink>
+                <div className={click ? "nav-menu active" : "nav-menu"}>
+                    <ul className="nav-list">
+                        <li className="nav-item">
+                            <NavLink
+                                exact
+                                to="/"
+                                activeClassName="active"
+                                className="nav-link"
+                                onClick={() => setClick(false)}
+                            >
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink
+                                exact
+                                to="/about"
+                                activeClassName="active"
+                                className="nav-link"
+                                onClick={() => setClick(false)}
+                            >
+                                About
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink
+                                exact
+                                to="/menu"
+                                activeClassName="active"
+                                className="nav-link"
+                                onClick={() => setClick(false)}
+                            >
+                                Menu
+                            </NavLink>
+                        </li>
+                    </ul>
+                    {click ? 
+                        <button className="mobile-btn">
+                            <Link
+                                exact
+                                to="/menu"
+                            >
+                                Order Now
+                            </Link>
+                        </button> : null }
+                </div>
+                <button className="nav-btn">
+                    <Link
+                        exact
+                        to="/menu"
+                    >
+                        Order Now
+                    </Link>
+                </button> 
+                <div className="nav-icon" onClick={handleClick}>
+                    {click ? <RiCloseLine /> : <RiMenu3Line />}
+                </div>
+            </nav>
+            {/* <nav className="navbar">
             <div className="nav-container">
                 <NavLink exact to="/" className="nav-logo">
                     Tamales Vigil
@@ -66,9 +131,9 @@ const Navbar = () => {
                         </button>
                     </div>
                 </div>
-                
             </div>
-        </nav>
+        </nav> */}
+        </header>
     );
 }
 
