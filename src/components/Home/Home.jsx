6b177729c195ddae41d48dd541ff76f25ff5618e
lizmery <l.vigil096@gmail.com>
2022-01-20 10:09:49 -0800

    Update Home page
    Update Home.jsx, Home.css, and upload hero images

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,66 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
+import Slider from 'react-slick'
+import { RiArrowLeftSLine, RiArrowRightSLine } from 'react-icons/ri'
 import './Home.css'
 
 const Home = () => {
-    return (
-        <section className="home">
-            <div className="home-container">
-                <h1>Authentic Tamales</h1>
-                <h6>A taste of Oaxaca, made with the utmost care</h6>
-                <Link to="/about" className="home-btn">Learn More</Link>
+    const NextArrow = ({ onClick }) => {
+        return (
+            <div className="arrow next" onClick={onClick}>
+                <RiArrowRightSLine />
             </div>
-        </section>
+        );
+    }
+
+    const PrevArrow = ({ onClick }) => {
+        return (
+            <div className="arrow prev" onClick={onClick}>
+                <RiArrowLeftSLine />
+            </div>
+        );
+    }
+
+    const settings = {
+        infinite: true,
+        lazyLoad: true,
+        speed: 300,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+    }
+    return (
+        <>
+            
+            <Slider {...settings}>
+                <div className="home one">
+                    <div className="home-container">
+                        <h1>Authentic Tamales</h1>
+                        <h6>A taste of Oaxaca, made with the utmost care</h6>
+                        <Link to="/about" className="home-btn">Learn More</Link>
+                    </div>
+                </div>
+                <div className="home two">
+                    <div className="home-container">
+                        <h1>Second Title</h1>
+                        <h6>Optio possimus omnis ad expedita dolore consequatur quo aut voluptas.</h6>
+                        <Link to="/about" className="home-btn">Learn More</Link>
+                    </div>
+                </div>
+                <div className="home three">
+                    <div className="home-container">
+                        <h1>The Third Title</h1>
+                        <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit!</h6>
+                        <Link to="/about" className="home-btn">Learn More</Link>
+                    </div>
+                </div>
+            </Slider>
+
+        </>
     )
 }
 
