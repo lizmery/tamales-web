99d78599f78c621d5631a701c73192a6a0693924
lizmery <l.vigil096@gmail.com>
2022-01-27 12:10:31 -0800

    Update Navbar.jsx
    remove dead code from Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -74,65 +74,6 @@ const Navbar = () => {
                     {click ? <RiCloseLine /> : <RiMenu3Line />}
                 </div>
             </nav>
-            {/* <nav className="navbar">
-            <div className="nav-container">
-                <NavLink exact to="/" className="nav-logo">
-                    Tamales Vigil
-                </NavLink>
-
-                <div className="nav-icon" onClick={handleClick}>
-                    {click ? <RiCloseLine /> : <RiMenu3Line />}
-                </div>
-
-                <div className={click ? "nav-menu active" : "nav-menu"}>
-                    <ul className="nav-items">
-                        <li className="nav-item">
-                            <NavLink
-                                exact
-                                to="/"
-                                activeClassName="active"
-                                className="nav-links"
-                                onClick={handleClick}
-                            >
-                                Home
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink
-                                exact
-                                to="/about"
-                                activeClassName="active"
-                                className="nav-links"
-                                onClick={handleClick}
-                            >
-                                About
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink
-                                exact
-                                to="/menu"
-                                activeClassName="active"
-                                className="nav-links"
-                                onClick={handleClick}
-                            >
-                                Menu
-                            </NavLink>
-                        </li>
-                    </ul>
-                    <div className="nav-btn">
-                        <button>
-                            <Link
-                                exact
-                                to="/menu"
-                            >
-                                Order Now
-                            </Link>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </nav> */}
         </header>
     );
 }
